61e653cd29bda88a88993017d2c3cdda117431cc
Shanee11 <75279488+Shanee11@users.noreply.github.com>
2020-12-02 17:44:12 -0600

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,62 @@
+/**
+ * @author Shanee Barnes
+ * @version 1.0
+ */
+
+import package java.time.LocalDateTime;
+
+public class Employee{
+    private int empID;
+    private String firstName;
+    private String lastName ;
+    private String pword;
+
+
+    /**
+     * Constructer for an empoyee object.
+     * @param empID - identification number for an Employee
+     * @param firstName - employee first name
+     * @param lastName - employee surname 
+     * @param Pword - employee password
+     */
+
+    public Employee(int empID, String firstName, String lastName, String pword, int timeIn, int timeOut){
+        this.empID = empID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pword = pword;
+    }
+
+
+    public int getEmpId(){
+        return this.empID;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+
+
+    public String getLastName() {
+        return this.lastName;
+    }
+ 
+
+    public int getPassword(){
+        return this.pword;
+    }
+
+
+    public void clockIn(LocalDateTime timeIn){
+    timeIn = LocalDateTime.now();
+    System.out,println(timeIn);
+    }
+
+    public void clockOut(LocalDateTime timeOut){
+    timeOut = LocalDateTime.now();
+    System.out,println(timeOut);
+    }
+
+
+}
