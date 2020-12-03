8cbf6185cc7d00091a6df713092d611ba32d9024
Comp2140Group <75224572+Comp2140Group@users.noreply.github.com>
2020-12-02 20:47:08 -0500

    Add files via upload

---
+++ comp2140Code/comp2140Code/InventoryManagement.java
@@ -0,0 +1,95 @@
+/**
+ * @author David Byfield
+ * @version 1.0
+ */
+
+package comp2140Code;
+
+
+import java.io.*;
+import java.util.*;
+
+import javax.swing.JOptionPane;
+
+
+
+public class InventoryManagement{
+    String filepath = "Item_Invetory.txt";
+    File inventory = new File(filepath);
+    
+     
+    
+    /**
+     * Adds an item to the inventory.
+     * 
+     * @param item obect of item class to be added
+     * @throws IOException
+     */
+    public void addItem(Item item) throws IOException {
+        
+        FileWriter add = new FileWriter(filepath, true);
+        try{
+            if (!inventory.createNewFile()){
+                String toAdd = item.toString();
+                add.write(toAdd);
+                add.close();
+            }
+            
+        } catch(IOException e){
+            JOptionPane.showMessageDialog(null, "Error");
+            e.printStackTrace();
+        }
+    }
+ 
+    /**
+     * Removes an item from the inventory
+     * @param item object of item class to be removed
+     */
+    public void removeItem(Item item) throws IOException {
+        String newFile = "temp.txt";
+        File tempFile = new File(newFile);
+        try{
+            FileWriter add = new FileWriter(filepath, true);
+            BufferedWriter bw = new BufferedWriter(add);
+            PrintWriter pw = new PrintWriter(bw);
+            Scanner readF = new Scanner(inventory);
+            readF.useDelimiter("[,\n]");
+            
+            if (!inventory.createNewFile()){
+                String id = String.valueOf(item.getId());
+                while (readF.hasNext()){
+                    String productID = readF.next();
+                    String productName = readF.next();
+                    String productAmount = readF.next();
+                    String productPrice = readF.next();
+                    if (!id.equals(productID)){
+                        pw.println(productID +","+productName+","+
+                        productAmount+","+productPrice);
+                    }
+            
+    
+                    
+
+                }
+            readF.close();
+            pw.flush();
+            pw.close();
+            add.close();
+            bw.close();
+            inventory.delete();
+            File dump = new File(filepath);
+            tempFile.renameTo(dump);
+                
+            }
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error");
+            e.printStackTrace();
+        }
+        
+
+        
+    }
+
+    
+    
+}
---
+++ comp2140Code/comp2140Code/TransMgmt.java
@@ -0,0 +1,86 @@
+/**
+*@author Malik Kington
+*@version 1.0
+*/
+
+import java.util.*;
+import java.io.*;
+
+package comp2140Code;
+
+//	NEEDDS TO IMPORT ITEM CLASS
+//	IMPLEMENT ADMIN PASSWORD FEATURE TO REMOVE ITEMS
+
+public class TransMgmt{
+
+	Invoice inv = new Invoice();
+	Customer customer = new Customer();
+//	GET ADMIN INFO FROM TEXT
+//	Admin admin = new Admin();
+	String filepath = "Transactions.txt";
+	File transactions = new File(filepath);
+
+	/**
+	*Adds an item to the invoice
+	*@param item object to be added to invoice
+	*/
+	public void addItem(Item item){
+		inv.addItem(item);
+	}
+
+	/**
+	*Removes an item from the invoice
+	*@param item object to be removed from invoice
+	*@param pword admin password
+	*/
+	public void removeItem(int itemID, String pword){
+		
+		inv.removeItem(itemID);
+	}
+
+	/**
+	*Begins the Checkout process for a transaction
+	*/
+	public void checkOut() throws IOException{
+		try{
+			String cash = "Cash";
+			String credit = "Credit";
+			Scanner check = new Scanner(System.in);
+			FileWriter tw = new FileWriter(filepath, true);
+			String i;
+
+			System.out.println("\nCustomer First Name: ");
+			i = check.next();
+			customer.setFName(i);
+			System.out.print("\nCustomer Last Name: ");
+			i = check.next();
+			customer.setLName(i);
+			System.out.print("\nCustomer Contact: ");
+			i = check.next();
+			customer.setContact(i);
+			System.out.println();
+
+			inv.setDate();
+			inv.toString();
+			System.out.print("Cash or Credit: ");
+			String type = check.next();
+			if (cash.equalsIgnoreCase(type)){
+				inv.setType(cash);
+				tw.write(customer.toString()+"\n"+inv.toString()+"\n "+inv.getType());
+				tw.close();
+				System.out.println("Checkout Successful");
+			}
+			else if (credit.equalsIgnoreCase(type)){
+				inv.setType(credit);
+				tw.write(customer.toString()+"\n"+inv.toString()+"\n"+inv.getType());
+				tw.close();
+				System.out.println("Checkout Successful");
+			}
+			System.out.println("Error. Check out again.");
+		}
+		catch (Exception e){
+			System.out.println("Error");
+			e.printStackTrace();
+		}
+	}
+}
