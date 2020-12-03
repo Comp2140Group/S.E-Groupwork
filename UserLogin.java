3cf701b7c7cf0ae7e52ea979a7e04a6ebb1b3b0b
Jojo578 <75263539+Jojo578@users.noreply.github.com>
2020-12-02 20:20:42 -0500

    Update UserLogin.java

--- UserLogin.java
+++ UserLogin.java
@@ -1,23 +1,12 @@
 
-import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
+import javax.swing.*;
 
 public class UserLogin extends JFrame {
 
