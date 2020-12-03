5e0c1f12329bc9e87adeb66f84ad2d68709026ae
Kington7 <74794658+Kington7@users.noreply.github.com>
2020-12-02 19:59:33 -0500

    Add files via upload

--- Cashier Module/src/Customer.java
+++ Cashier Module/src/Customer.java
@@ -1,39 +1,120 @@
+/**
+*@author Malik Kington
+*@version 1.0
+*/
+
 import java.util.*;
 
 public class Customer{
 	
-	private int cmrID;
 	private ArrayList<Invoice> inv = new ArrayList<Invoice>();
 	private String fName;
 	private String lName;
-
-	public Customer(int cmrID, String fName, String lName){
-		this.cmrID = cmrID;
+	private String contact;
+	
+	/**
+	*Constructor for a customer object
+	*@param fName customer's first name
+	*@param lName customer's last name
+	*@param contact customer's contact information
+	*@param inv arraylist storing customer's invoices
+	*/
+	public Customer(String fName, String lName, String contact){
+		this.contact = contact;
 		this.fName = fName;
 		this.lName = lName;
 	}
 
+	/**
+	*Null constructor for a customer object
+	*/
+	public Customer(){
+
+	}
+
+	/**
+	*Retrieves first name of customer
+	*@return first name of customer
+	*/
 	public String getFName(){
 		return fName;
 	}
-
+	
+	/**
+	*Retrieves last name of customer
+	*@return last name of customer
+	*/
 	public String getLName(){
 		return lName;
 	}
-
-	public int getID(){
-		return cmrID;
+	
+	/**
+	*Retrieves customer contact info
+	*@return customer contact info
+	*/
+	public String getContact(){
+		return contact;
 	}
 
+	
+	/**
+	*Adds an invoice to the arraylist
+	*@param inv invoice to be added to arraylist
+	*/
 	public void addInv(Invoice inv){
 		this.inv.add(inv);
 	}
 
+	/**
+	*Updates customer's first name
+	*@param fName customer's first name
+	*/
 	public void setFName(String fName){
 		this.fName = fName;
 	}
-
+	
+	/**
+	*Updates customer's last name
+	*@param lName customer's last name
+	*/
 	public void setLName(String lName){
 		this.lName = lName;
+	}
+	
+	/**
+	*Updates customer's contact information
+	*@param contact customer's contact information
+	*/
+	public void setContact(String contact){
+		this.contact = contact;
+	}
+
+	/**
+	*Converts Customer object to String
+	*@return customer details as String
+	*/
+	public String toString(){
+		System.out.println(fName+" "+lName);
+		System.out.println("Contact: "+contact);
+		return null;
+	}
+
+	/**
+	*Converts Customer object to String with invoices
+	*@param l string "all" to override toString()
+	*@return customer details and invoices as String
+	*/
+	public String toString(String l){
+		if (l.equalsIgnoreCase("All")){
+			System.out.println(fName+" "+lName);
+			System.out.println("Contact: "+contact);
+			System.out.println();
+			int i = 0;
+			while (i<inv.size()){
+				inv.get(i).toString();
+				i++;
+			}
+		}
+		return null;
 	}
 }
--- Cashier Module/src/Invoice.java
+++ Cashier Module/src/Invoice.java
@@ -1,3 +1,8 @@
+/**
+*@author Malik Kington
+*@version 1.0
+*/
+
 import java.util.*;
 
 public class Invoice{
@@ -6,43 +11,82 @@ public class Invoice{
 	private boolean type;
 	private double subTotal;
 	private double total;
-	private Date date = new Date();
-	private int invNum;
-//	CHECK SALES TAX	
+	private Date date;
+	private static int invNum = 0;
 	private final double TAX = 0.165;
-
-	public Invoice(int invNum){
-		this.invNum = invNum;
+	
+	/**
+	*Constructor for a invoice object
+	*/
+	public Invoice(){
+		invNum++;
 	}
-
+	
+	/**
+	*Retrieves the nature of transaction: cash or credit
+	*@return String "Cash" or "Credit"
+	*/
 	public String getType(){
 		if (type = true){
 			return "Cash";
 		}
 		return "Credit";
 	}
-
+	
+	/**
+	*Retrieves total cost of items after tax
+	*@return total cost of items after tax
+	*/
 	public double getTotal(){
 		return total;
 	}
-
+	
+	/**
+	*Retrieves total cost of items before tax
+	*@return total cost of items before tax
+	*/
 	public double getSubToal(){
 		return subTotal;
 	}
-
+	
+	/**
+	*Retrieves the date of the invoice
+	*@return the date of the invoice
+	*/
 	public Date getDate(){
 		return date;
 	}
-
+	
+	/**
+	*Retrieves invoice number
+	*@return invoice number
+	*/
 	public int getInvNum(){
 		return invNum;
 	}
 
-	public void setDate(Date date){
-//	NEEDS TO BE LOOKED AT
-		this.date = date;
+	/**
+	*Sets the date that the invoice was made
+	*/
+	public void setDate(){
+		this.date = new Date();
 	}
 
+	/**
+	*Sets the date that the invoice was made. Overrides setDate()
+	*@param year year input
+	*@param month month input
+	*@param day day input
+	*@param hr hour input
+	*@param min minute input
+	*/
+	public void setDate(int year, int month, int day, int hr, int min){
+		this.date = new Date(year, month, day, hr, min);
+	}
+
+	/**
+	*Calculates the Total before tax of the items on the invoice
+	*/
 	public void calcSubTotal(){
 		int i = 0;
 		double sum = 0;
@@ -53,11 +97,18 @@ public class Invoice{
 		this.subTotal = sum;
 	}
 
+	/**
+	*Calculates the Total after tax of the items on the invoice
+	*/
 	public void calcTotal(){
 		double tax = subTotal * TAX;
 		total = subTotal + tax;
 	}
 
+	/**
+	*Sets the transaction nature of the invoice
+	*@param type cash or credit as string
+	*/
 	public void setType(String type){
 		if (type.equalsIgnoreCase("Cash")){
 			this.type = true;
@@ -65,38 +116,50 @@ public class Invoice{
 		else if (type.equalsIgnoreCase("Credit")){
 			this.type = false;			
 		}
-//	MAYBE THIS SHOULD THROW EXCEPTION
-		else{
-			System.out.println("Error");
-		}
+		System.out.println("Error");
 	}
 
+	/**
+	*Adds an item to the invoice
+	*@param item object to be added to bill
+	*/
 	public void addItem(Item item){
 		bill.add(item);
 	}
-
+	
+	/**
+	*Removes an item from the invoice
+	*@param itemID ID of object to be removed from invoice
+	*/
 	public void removeItem(int itemID){
-//	IMPLEMENT ONE FOR NAME, DESCRIPTION, ETC.
 		int i = 0;
 		while(i<bill.size()){
-			if (bill.get(i).getID() = itemID){
+			if (bill.get(i).getId()==itemID){
 				i = bill.size();
 			}
 			i++;
 		}
 	}
 
+	/**
+	*Converts invoice object to string
+	*@return invoice details as string
+	*/
 	public String toString(){
+		System.out.println(invNum);
+		System.out.println(date+"\n");
 		int i = 0;
 		while (i<bill.size()){
-			System.out.println(bill.get(i).getName());
+			System.out.println(bill.get(i).getName()+' '+String.valueOf(bill.get(i).getPrice()));
 			i++;
 		}
 		calcSubTotal();
 		calcTotal();
 		String s = String.valueOf(subTotal);
+		String tx = String.valueOf(subTotal*TAX);
 		String t = String.valueOf(total);
 		System.out.println("Subtotal: " + s);
+		System.out.println("Sales Tax: " + tx);
 		System.out.println("Total: " + t);
 		return null;
 	}
